900313eef97ed062b380f5a9875bea3e78b9f78a
JuliaNedilchak <julianedilchak@gmail.com>
2024-08-08 21:21:58 +0100

    final

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 import pino from 'pino-http';
 import express from 'express';
 //import { Contact } from './db/contacts.js';
-import { getAllCOntacts } from './services/contactRequest.js';
+import { getAllCOntacts, getContactById } from './services/contactRequest.js';
 
 //dotenv.config();
 
@@ -10,6 +10,7 @@ const PORT = Number(process.env.PORT) || 3000;
 
 export function setupServer() {
   const app = express();
+
   app.use(cors());
   app.use(
     pino({
@@ -23,7 +24,27 @@ export function setupServer() {
     try {
       const contacts = await getAllCOntacts();
       console.log(contacts);
-      res.status(200).json({ data: contacts });
+      res.status(200).json({
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+      if (contact === null) {
+        return res.status(404).json({
+          message: 'Contact is not found',
+        });
+      }
+      res.status(200).json({
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
     } catch (error) {
       console.log(error);
     }
