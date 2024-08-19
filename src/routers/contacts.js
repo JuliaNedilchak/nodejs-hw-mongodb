4a0ccf46542c0bdda8663a7386900180f7fadaeb
JuliaNedilchak <julianedilchak@gmail.com>
2024-08-19 22:57:53 +0900

    final

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,14 +1,24 @@
 import { Router } from 'express';
-
+import express from 'express';
 import {
   getAllContactsController,
   getContactByIdController,
+  CreateContactController,
+  deleteContactController,
+  patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
+const jsonParser = express.json();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-
+router.post('/contacts', jsonParser, ctrlWrapper(CreateContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.patch(
+  '/contacts/:contactId',
+  jsonParser,
+  ctrlWrapper(patchContactController),
+);
 export default router;
--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ export function setupServer() {
       },
     }),
   );
+  //app.use(express.json());
   app.use(contactsRouter);
 
   app.use('*', notFoundHandler);
